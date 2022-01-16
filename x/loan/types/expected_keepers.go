48a3f3d44bec4e6b2b6cb1be49cbeb1df5ddafc0
uwezukwechibuzor <uwezukwechibuzor@gmail.com>
2022-01-16 03:10:49 +0100

    Add request-loan message

--- x/loan/types/expected_keepers.go
+++ x/loan/types/expected_keepers.go
@@ -1,5 +1,7 @@
 package types
 
+import sdk "github.com/cosmos/cosmos-sdk/types"
+
 type BankKeeper interface {
-	// Methods imported from bank should be defined here
+	SendCoinsFromAccountToModule(ctx sdk.Context, senderAddr sdk.AccAddress, recipientModule string, amt sdk.Coins) error
 }
